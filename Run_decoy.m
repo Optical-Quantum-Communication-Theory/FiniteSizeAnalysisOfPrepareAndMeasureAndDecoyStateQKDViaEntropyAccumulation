4a9539af5cece5f8e3f0fe69b032f9d5f12a2dec
LKamin96 <lkamin@uwaterloo.ca>
2024-07-25 15:51:36 -0400

    Added two functions generating the plots

---
+++ Run_decoy.m
@@ -0,0 +1,47 @@
+%Transmitivity
+eta_List = 10.^(-linspace(0,4,21));
+eta_List(1) = 10^(-2);
+
+%Error correction efficiency
+f_EC=1.16;
+
+%Misalignment (only used for decoy)
+misalignment = 0.01;
+
+%Depolarization
+depol = 0.01;
+
+%Security parameter
+epsilon_sec = 1e-8;
+
+% Decoy intensities
+decoy_intens = [0.9,0.02,0.001];
+
+%Probabilities of sending each decoy intensity given a test round, i.e
+%p(decoy|test)
+decoy_probs = [1/3,1/3,1/3];
+
+%Photonnumber cutoff
+n_photon = 10;
+
+%List of Exponent of number of signals sent, i.e. n = 10^(n_signals)
+N_list = [7,8,10,12];
+
+%% run the plot
+
+%Store empty matrix of key rates
+KeyRate = zeros(N_list,numel(eta_List));
+
+for indexSignal = 1:numel(N_list)
+    for indexEta = 1:numel(eta_List)
+        %N signal 
+        n_signals = N_list(indexSignal);
+        %transmittivity
+        eta = eta_List(indexEta);
+        [optdeltacomp_decoy, optkeydecoy] = DecoyBB84_keyrate_EAT(eta, f_EC, misalignment, n_signals, epsilon_sec,decoy_intens,decoy_probs,n_photon);
+        %store result in matrix. Each row corresponds to one value of
+        %n_signal
+        KeyRate(indexSignal,indexEta) = optkeydecoy;
+    end
+end
+
---
+++ Run_qubit.m
@@ -0,0 +1,34 @@
+%Transmitivity
+eta_List = 10.^(-linspace(0,4,21));
+eta_List(1) = 10^(-2);
+
+%Error correction efficiency
+f_EC=1.16;
+
+%Depolarization
+depol = 0.01;
+
+%Security parameter
+epsilon_sec = 1e-8;
+
+%List of Exponent of number of signals sent, i.e. n = 10^(n_signals)
+N_list = [6,8,10,12];
+
+%% run the plot
+
+%Store empty matrix of key rates
+KeyRate = zeros(N_list,numel(eta_List));
+
+for indexSignal = 1:numel(N_list)
+    for indexEta = 1:numel(eta_List)
+        %N signal 
+        n_signals = N_list(indexSignal);
+        %transmittivity
+        eta = eta_List(indexEta);
+        [optdeltacomp_qubit, optkeyratequbit] = QubitBB84_keyrate_EAT (eta, f_EC, depol, n_signals, epsilon_sec);
+        %store result in matrix. Each row corresponds to one value of
+        %n_signal
+        KeyRate(indexSignal,indexEta) = optkeyratequbit;
+    end
+end
+
